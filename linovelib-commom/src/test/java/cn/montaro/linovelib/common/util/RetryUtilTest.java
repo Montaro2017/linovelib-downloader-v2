143e07e5a37a86190ee5ecf1c5b7aad0cc96d178
Sparks <kertlk@qq.com>
2022-05-29 14:35:38 +0800

    test: 重试机制测试

---
+++ linovelib-commom/src/test/java/cn/montaro/linovelib/common/util/RetryUtilTest.java
@@ -0,0 +1,17 @@
+package cn.montaro.linovelib.common.util;
+
+import cn.hutool.http.HttpUtil;
+import org.junit.jupiter.api.Test;
+
+public class RetryUtilTest {
+
+    @Test
+    public void testRetry() {
+        String url = "asljdlkj";
+        long before = System.currentTimeMillis();
+        String content = RetryUtil.retry(() -> HttpUtil.get(url), 3, 1000);
+        long after = System.currentTimeMillis();
+        System.out.println("content = " + content);
+        System.out.println("(after-before) = " + (after - before));
+    }
+}
