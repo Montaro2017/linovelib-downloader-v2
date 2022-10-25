61eb44a172de65be99bc29a0ba5b4c9c85898420
zhangjiayu <kertlk@qq.com>
2022-10-25 17:29:30 +0800

    release: v1.0.1

--- linovelib-cli/src/main/java/cn/montaro/linovelib/cli/Main.java
+++ linovelib-cli/src/main/java/cn/montaro/linovelib/cli/Main.java
@@ -30,7 +30,7 @@ import java.util.List;
 @Slf4j
 public class Main {
 
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     public static final String GIT_URL = "https://gitee.com/Montaro2017/linovelib-downloader-v2";
 
