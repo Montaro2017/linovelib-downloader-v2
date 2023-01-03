d61feff70b19e6a6341fd44265d70c0a6d5b5bc0
montaro <kertlk@qq.com>
2023-01-03 20:55:20 +0800

    release: v1.0.2

--- linovelib-cli/src/main/java/cn/montaro/linovelib/cli/Main.java
+++ linovelib-cli/src/main/java/cn/montaro/linovelib/cli/Main.java
@@ -30,7 +30,7 @@ import java.util.regex.Matcher;
 @Slf4j
 public class Main {
 
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     public static final String GIT_URL = "https://gitee.com/Montaro2017/linovelib-downloader-v2";
 
